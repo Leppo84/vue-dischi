aa5b22be42b6422482a4bcd978c98c0cf22c06cc
LeppoXena <francesco.lepore@xenagames.com>
2022-06-13 16:37:34 +0200

    selector added in app

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,6 +1,16 @@
 <template>
     <header>
         <img alt="Spotify logo" src="../assets/Logo Spotify.png">
+        <div class="filter-album">
+            <span>Seleziona il genere</span>
+            <select name="genre-selector" id="genre-selector">
+                <option id="opt_all" selected="selected" value="all">All</option>
+                <option id="opt_jazz" value="jazz">Jazz</option>
+                <option id="opt_metal" value="metal">Metal</option>
+                <option id="opt_pop" value="pop">Pop</option>
+                <option id="opt_Rock" value="rock">Rock</option>
+            </select>
+        </div>
     </header>
 </template>
 
@@ -19,12 +29,30 @@ header {
     background-color: #2e3a46;
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
 
     img {
         width: 60px;
         padding: 8px;
         margin: 0 16px;
     }
+
+    div.filter-album {
+        padding: 8px;
+        margin: 0 16px;
+
+        span {
+            display: inline-block;
+            color: white;
+            margin-right: 16px;
+        }
+        select {
+            width: 150px;
+        }
+
+    }
 }
 
 </style>
+
